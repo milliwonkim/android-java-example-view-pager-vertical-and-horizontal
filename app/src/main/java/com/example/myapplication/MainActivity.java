c38e8704c643bc2ae75e8024f368c9d043aa756d
milliwonkim <rldnjs9347@gmail.com>
2020-11-08 11:44:17 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+    Button btnToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+        btnToggle = findViewById(R.id.btnToggle);
+
+        ArrayList<DataPage> list = new ArrayList<>();
+        list.add(new DataPage(android.R.color.black, "1 Page"));
+        list.add(new DataPage(android.R.color.holo_red_light, "2 Page"));
+        list.add(new DataPage(android.R.color.holo_green_light, "3 Page"));
+        list.add(new DataPage(android.R.color.holo_orange_dark, "4 Page"));
+        list.add(new DataPage(android.R.color.holo_blue_light, "5 Page"));
+        list.add(new DataPage(android.R.color.holo_blue_dark, "6 Page"));
+
+        viewPager2.setAdapter(new ViewPagerAdapter(list));
+
+        btnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(viewPager2.getOrientation() == ViewPager2.ORIENTATION_VERTICAL) {
+                    btnToggle.setText("가로로 슬라이드");
+                    viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+                } else {
+                    btnToggle.setText("세로로 슬라이드");
+                    viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+                }
+            }
+        });
+    }
+}
